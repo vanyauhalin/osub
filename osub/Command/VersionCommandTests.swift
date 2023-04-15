56d6339981a6f68a879f5e6579d33b66e25615d3
vanyauhalin <vanyauhalin@gmail.com>
2023-04-15 21:39:34 +0400

    Bump to v0.2.0

--- osub/Command/VersionCommandTests.swift
+++ osub/Command/VersionCommandTests.swift
@@ -7,14 +7,14 @@ final class VersionCommandTests: XCTestCase {
   func testRuns() throws {
     let output = MockedTextOutputStream()
     var command = try VersionCommand.parse([])
-    command.bundle = MockedBundle(infoDictionary: ["CFBundleVersion": "0.1.0"])
+    command.bundle = MockedBundle(infoDictionary: ["CFBundleVersion": "0.2.0"])
     command.output = output
     command.run()
     XCTAssertEqual(
       output.string,
       """
-      0.1.0
-      https://github.com/vanyauhalin/osub/releases/tag/v0.1.0\n
+      0.2.0
+      https://github.com/vanyauhalin/osub/releases/tag/v0.2.0\n
       """
     )
   }
