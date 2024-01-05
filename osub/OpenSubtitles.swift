2034bc68d2a753f03813f157cdb44564395c5adb
vanyauhalin <vanyauhalin@gmail.com>
2024-01-05 17:22:33 +0400

    ah shit, here we go again

--- osub/OpenSubtitles.swift
+++
@@ -1,7 +0,0 @@
-import ArgumentParser
-import Command
-
-@main
-struct OpenSubtitles: AsyncParsableCommand {
-  static let configuration = Command.configuration
-}
--- osub/TestCase/Command.swift
+++
@@ -1,9 +0,0 @@
-import Command
-
-public final class MockedTextOutputStream: StandardTextOutputStream {
-  public var string = ""
-
-  override public func write(_ string: String) {
-    self.string += string
-  }
-}
